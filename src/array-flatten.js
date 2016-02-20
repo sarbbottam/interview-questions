cea185141f9ad707cc19b7edf6ce981071308880
Sarbbottam Bandyopadhyay <sarbbottam@gmail.com>
2016-02-19 22:24:23 -0800

    using spread operator (...) instead of [].concat

--- src/array-flatten.js
+++ src/array-flatten.js
@@ -8,11 +8,11 @@ export const iteratively = (array) => {
   for(let i = 0, l = array.length; i < l; i += 1) {
     if( Array.isArray(array[i]) ) {
       // if the item at ith index is an array
-      array = Array.prototype.concat(
-        array.slice(0, i), // portion of the before the ith index 
-        array[i], // the item of ith index, which is an array
-        array.slice(i + 1, l) // portion of the after the ith index
-      );
+      array = [
+        ...array.slice(0, i), // portion of the before the ith index 
+        ...array[i], // the item of ith index, which is an array
+        ...array.slice(i + 1, l) // portion of the after the ith index
+      ];
       l = array.length; // update the value of l as length of the array has increased
       i -= 1; // the item at current ith idex could be an array    
     }
