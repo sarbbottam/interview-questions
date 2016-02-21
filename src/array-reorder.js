17131f67ae974b8dadd42c070627bb58e121f11b
Sarbbottam Bandyopadhyay <sarbbottam@gmail.com>
2016-02-20 17:23:36 -0800

    added array-reorder function

---
+++ src/array-reorder.js
@@ -0,0 +1,23 @@
+/*
+ * a funtion that takes two array as arguments and reorder the array
+ * with respect to the order
+ * array = ['C', 'D', 'E', 'F', 'G'];
+ * order = [3, 0, 4, 1, 2];
+ * result = ['D', 'F', 'G', 'C', 'E'];
+ */
+
+export default (array, order) => {
+  
+  for( let i = 0, l = order.length; i < l; i += 1) {
+    if(order[i] !== i) {
+      let orderIndex = order[i];
+      order[i] = order[orderIndex];
+      order[orderIndex] = orderIndex;
+      
+      let temp = array[i];
+      array[i] = array[orderIndex];
+      array[orderIndex] = temp;
+    }
+  }
+  return array;    
+};
---
+++ test/array-reorder.js
@@ -0,0 +1,13 @@
+import {assert} from 'chai';
+import reorder from '../src/array-reorder';
+
+describe('array-reorder', () => {
+  
+  it(`should reorder the array as per given order`, () => {  
+    assert.deepEqual(
+      ['D', 'F', 'G', 'C', 'E'], 
+      reorder(['C', 'D', 'E', 'F', 'G'], [3, 0, 4, 1, 2])
+    );
+  });
+  
+});
