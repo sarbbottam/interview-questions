1222bfac536638e8c80ee23fdb881ea8d3df24ba
Sarbbottam Bandyopadhyay <sarbbottam@gmail.com>
2016-02-20 16:14:05 -0800

    added string-repeat function

---
+++ src/string-repeat.js
@@ -0,0 +1,16 @@
+/*
+ * a function that will repeat a string as per the count provided 
+ */
+export default (string, count) => {
+  let result = '';
+  while(count > 0) {
+    if(count % 2 === 1) {
+      result += string;
+      count -= 1;
+    } else {
+      string += string;
+      count /= 2;
+    }   
+  }
+  return result;    
+};
